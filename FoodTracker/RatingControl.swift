180fc56bcaab50e33e00efa9eab1061918588e03
Adam Rynczak <tiny@Adams-MacBook-Pro.local>
2015-12-01 18:08:06 -0500

    Added rating controller.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Adam Rynczak on 12/1/15.
+//  Copyright © 2015 Adam Rynczak. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+}
